0b52000661c4c03cbc44ea4ee5e0da36051fdb9d
NHTanvir <naymulhasantanvir10@gmail.com>
2025-07-18 19:37:23 +0600

    favrouite query addeed

--- inc/functions.php
+++ inc/functions.php
@@ -18,3 +18,22 @@ function pc_site_url() {
 	return $url;
 }
 endif;
+add_action('elementor/query/filter_by_post_ids', function($query) {
+    if ( isset($_GET['show_favorites']) && $_GET['show_favorites'] == '1' ) {
+        if ( is_user_logged_in() ) {
+            $user_id = get_current_user_id();
+            $favorites = get_user_meta($user_id, 'b2b_favorites', true);
+
+            if ( is_array($favorites) && !empty($favorites) ) {
+                $query->set('post_type', 'voiture');
+                $query->set('post__in', $favorites);
+                $query->set('orderby', 'post__in');
+                $query->set('posts_per_page', -1);
+            } else {
+                $query->set('post__in', [0]);
+            }
+        } else {
+            $query->set('post__in', [0]);
+        }
+    }
+});
